338d4e9d1807b8b2b9267c2971248960487a7261
ZORO7861 <prootaku786@gmail.com>
2025-04-08 02:59:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", "21803165"))
-API_HASH = getenv("API_HASH", "05e5e695feb30e25bef47484cc006da7")
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", "7403621976"))
-MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "Purvi_UPdates")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "PURVI_SUPPORT")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "ll_ALPHA_BABY_lll")
+API_ID = int(getenv("API_ID", "23012237"))
+API_HASH = getenv("API_HASH", "c90dc14145de6fe292d1f7034861993a")
+BOT_TOKEN = getenv("BOT_TOKEN", "7007221986:AAHX004OXGlHO00UcJzsa9GQywMfZkwLa4A")
+OWNER_ID = int(getenv("OWNER_ID", "8019277081"))
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://SUPERBOTS:PROOTAKU@cluster0.k1etl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+SUPPORT_GRP = getenv("SUPPORT_GRP","DEMON_X_SUPPORT")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "DEMON_X_SUPPORT")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "Pro_otaku")
 
 # Random Start Images
 IMG = [
